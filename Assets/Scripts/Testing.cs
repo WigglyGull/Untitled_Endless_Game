ec8ba99e3b35d29890efa3370699734cb6507803
Auron Walker <auronwalkeris@gamil.com>
2019-12-13 14:18:31 +1100

    new commit

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Testing : MonoBehaviour{
-    [SerializeField] Ui ui;
+    public Ui ui;
 
     void Awake() {
         PlayerStats playerStats = new PlayerStats();
---
+++ Assets/Scripts/Utls/Assets.cs
@@ -0,0 +1,35 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading the Code Monkey Utilities
+    I hope you find them useful in your projects
+    If you have any questions use the contact form
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CodeMonkey {
+    public class Assets : MonoBehaviour {
+
+        private static Assets _i; 
+
+        public static Assets i {
+            get {
+                if (_i == null) _i = Instantiate(Resources.Load<Assets>("CodeMonkeyAssets")); 
+                return _i; 
+            }
+        }
+
+        public Sprite s_White;
+        public Sprite s_Circle;
+
+        public Material m_White;
+    }
+
+}
